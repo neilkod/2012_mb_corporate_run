9cabe74a1f3ac853693433e930504ef06bf2a7fa
neil kodner <nkodner@gmail.com>
2012-03-31 20:48:23 -0400

    added some more charts in R

--- analyze_2012_5k.R
+++ analyze_2012_5k.R
@@ -20,6 +20,27 @@ summary(cbsi_male$minutes)
 
 summary(cbsi_female$minutes)
 
-
+# boxplot of cbsi male vs female finishers
 p <- ggplot(cbsi_runners,aes(gender,minutes))
 p + geom_boxplot(aes(alpha=.4,fill=gender)) + coord_flip() + geom_point(shape=4,size=6,aes(colour=cbsi_runners$gender)) + opts(title="Finishing times for CBSi 5k participants - 2012 Mercedes-Benz Corporate Challenge, Ft. Lauderdale")
+
+# cbsi male-female density plot
+m <- ggplot(cbsi_runners,aes(x=minutes))
+m + geom_density(aes(alpha=.65,fill=factor(gender))) + scale_x_continuous(limits =c(10,90)) + opts(title="Distribution of finishing times for CBSi 5k participants")
+
+# density plot of all participants
+x <- ggplot(raw_data,aes(x=minutes))
+x + geom_density(aes(alpha=.65,fill=factor(gender))) + scale_x_continuous(limits =c(10,90)) + opts(title="Distribution of finishing times for all 5k participants")
+
+
+# find top 15 teams, by count
+teams <- ddply(raw_data,"team",function(dat) c(nrow(dat), median(dat$minutes)))
+names(teams) <- c('team','count','median_time_in_minutes')
+
+# facet with teams with > 25 members
+n=25
+top_teams <- teams[teams$count > n,]
+
+z <- ggplot(merge(raw_data,top_teams),aes(x=minutes))
+z + geom_density(aes(alpha=.65,fill=factor(gender))) + scale_x_continuous(limits =c(10,90)) + opts(title=paste("Distribution of M/F finishing times for companies > ", n, " runners")) + facet_wrap(~team, ncol=6)
+
