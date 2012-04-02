2349f82bb223721aff3c804a07e2d64207878c18
neil kodner <nkodner@gmail.com>
2012-04-02 11:58:41 -0400

    add ecdf

--- analyze_2012_5k.R
+++ analyze_2012_5k.R
@@ -15,6 +15,8 @@ cbsi_male <- cbsi_runners[cbsi_runners$gender == "M",]
 
 cbsi_female <- cbsi_runners[cbsi_runners$gender == "F",]
 
+
+
 # display the male and female summaries
 summary(cbsi_male$minutes)
 
@@ -34,8 +36,8 @@ x + geom_density(aes(alpha=.65,fill=factor(gender))) + scale_x_continuous(limits
 
 
 # find top 15 teams, by count
-teams <- ddply(raw_data,"team",function(dat) c(nrow(dat), median(dat$minutes)))
-names(teams) <- c('team','count','median_time_in_minutes')
+teams <- ddply(raw_data,"team",function(dat) c(nrow(dat), median(dat$minutes),mean(dat$minutes)))
+names(teams) <- c('team','count','median_time_in_minutes','mean_time_in_minutes')
 
 # facet with teams with > 25 members
 n=25
@@ -43,4 +45,31 @@ top_teams <- teams[teams$count > n,]
 
 z <- ggplot(merge(raw_data,top_teams),aes(x=minutes))
 z + geom_density(aes(alpha=.65,fill=factor(gender))) + scale_x_continuous(limits =c(10,90)) + opts(title=paste("Distribution of M/F finishing times for companies > ", n, " runners")) + facet_wrap(~team, ncol=6)
+
+# male/female specific team summaries. I don't like this approach
+male_runners <- raw_data[raw_data$gender == "M",]
+female_runners <- raw_data[raw_data$gender == "F",]
+male_team_stats <- ddply(male_runners,"team",function(dat) c(nrow(dat), median(dat$minutes),mean(dat$minutes)))
+names(male_team_stats) <- c('team','count','median_time_in_minutes','mean_time_in_minutes')
+
+female_team_stats <- ddply(female_runners,"team",function(dat) c(nrow(dat), median(dat$minutes),mean(dat$minutes)))
+names(female_team_stats) <- c('team','count','median_time_in_minutes','mean_time_in_minutes')
+
+all_team_summary<-merge(male_team_stats,female_team_stats,by="team")
+
+faster_women <- all_team_summary[all_team_summary$mean_time_in_minutes.x > all_team_summary$mean_time_in_minutes.y, ]
+q <- ggplot(merge(raw_data,faster_women),aes(x=minutes))
+q + geom_density(aes(alpha=.65,fill=factor(gender))) + scale_x_continuous(limits =c(10,90)) + opts(title="teams with faster women") + facet_wrap(~team, ncol=6)
+
+# male vs female mean finishing times
+qplot(all_team_summary,aes(x=mean_time_in_minutes.x,y=mean_time_in_minutes.y))
+
+#produce an ecdf
+plot(ecdf(raw_data$minutes),col="red",verticals=TRUE,main="ecdf of 5k finishers(red) and CBSi (orange)",sub="minutes")
+lines(ecdf(cbsi_runners$minutes),col="orange")
+
+
+
+
+
 
